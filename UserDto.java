a000f70b333955535f5850f19c3a8241d1e52088
Raguram Murugan <116337952+raguramMurugan@users.noreply.github.com>
2022-12-02 19:31:29 +0530

    Add files via upload

---
+++ UserDto.java
@@ -0,0 +1,15 @@
+package com.hubino.springSecurity.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDto {
+	private String firstname;
+	private String lastname;
+	private String password;
+	private String email;
+	
+
+}
